9c614fe6224a945ab5124eb36311f5afecd69535
aliyun <396431055@qq.com>
2022-07-21 20:18:37 +0800

    posix thread and echos/c

---
+++ lesson27-posix-thread/01thread.c
@@ -0,0 +1,46 @@
+#include<unistd.h>
+#include<sys/types.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+
+#define ERR_EXIT(m) \
+        do \
+        { \
+             perror(m); \
+             exit(EXIT_FAILURE);    \
+        } while (0)
+
+void* thread_routine(void *arg)
+{
+    for(int i = 0; i < 20; i++){
+        printf("B\n");
+        fflush(stdout);
+        usleep(20);
+    }
+    return 0;
+}
+
+int main(){
+
+    pthread_t tid;
+    int ret = pthread_create(&tid, NULL, thread_routine, NULL);
+    if(ret != 0)
+    {
+        fprintf(stderr, "pthread_create:%s \n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < 20; i++){
+        printf("A\n");
+        fflush(stdout);
+        usleep(20);
+    }
+
+    sleep(2);
+        
+    return 0;
+}
